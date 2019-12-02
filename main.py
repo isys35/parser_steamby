3d76213f41052ad9771f07192e0d3424a108b412
isys35 <isys35@mail.com>
2019-12-02 10:46:45 +0300

    Добавление декоратора для расчёта скорости выполнения функции

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.firefox.options import Options
+
 # for i in range(1, pages + 1):
 #     browser = webdriver.Firefox(options=options)
 #     browser.get("https://steambuy.com/catalog/?page=%i" % i)
@@ -16,12 +17,26 @@ from selenium.webdriver.firefox.options import Options
 #     browser.quit()
 
 
-url = 'https://steambuy.com/catalog/' # ссылка на каталог
+url = 'https://steambuy.com/catalog/'  # ссылка на каталог
 options = Options()
 options.headless = True
 
 
+def benchmark(func):
+    import time
+
+    def wrapper():
+        start = time.time()
+        return_value = func()
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end - start))
+        return return_value
+    return wrapper
+
+
+@benchmark
 def count_pages():
+    print('Определение кол-ва страниц...')
     browser = webdriver.Firefox(options=options)
     browser.get(url)
     # Получение HTML-содержимого
@@ -32,6 +47,7 @@ def count_pages():
     pages_list = []
     for page in pages:
         pages_list.append(page.text)
+    print('Кол-во страниц ' + pages_list[-1])
     return pages_list[-1]
 
 
