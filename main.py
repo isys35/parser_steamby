71930628df5e2f4e04eecd9d9b6b5512254134a0
isys35 <isys35@mail.com>
2019-12-02 11:01:04 +0300

    Парсинг по страницам

--- main.py
+++ main.py
@@ -47,12 +47,27 @@ def count_pages():
     pages_list = []
     for page in pages:
         pages_list.append(page.text)
+    browser.quit()
     print('Кол-во страниц ' + pages_list[-1])
     return pages_list[-1]
 
 
 def main():
-    print(count_pages())
+    pages = int(count_pages())
+    for i in range(1, pages + 1):
+        browser = webdriver.Firefox(options=options)
+        page_url = '?page=%i' % i
+        browser.get(url + page_url)
+        requiredHtml = browser.page_source
+        soup = BeautifulSoup(requiredHtml, 'html.parser')
+        product_list = soup.select('.product-item')
+        games = []
+        for product in product_list:
+            title = product.select('.product-item__title > a')
+            cost = product.select('.product-item__cost')
+            games.append({'title': title[0].text, 'cost': cost[0].text})
+        print(games[-1])
+        browser.quit()
 
 
 if __name__ == "__main__":
