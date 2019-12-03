8298b9765de348bb7b2c78f9e4378d49db29a6d5
isys35 <isys35@mail.com>
2019-12-03 15:52:47 +0300

    Парсинг по страницам

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.firefox.options import Options
 
-# for i in range(1, pages + 1):
-#     browser = webdriver.Firefox(options=options)
-#     browser.get("https://steambuy.com/catalog/?page=%i" % i)
-#     # Получение HTML-содержимого
-#     requiredHtml = browser.page_source
-#     soup = BeautifulSoup(requiredHtml, 'html.parser')
-#     product_list = soup.select('.product-item')
-#     for product in product_list:
-#         title = product.select('.product-item__title > a')
-#         cost = product.select('.product-item__cost')
-#         games.append({'title': title[0].text, 'cost': cost[0].text})
-#         print(games[-1])
-#     browser.quit()
-
-
 url = 'https://steambuy.com/catalog/'  # ссылка на каталог
 options = Options()
 options.headless = True
@@ -52,22 +37,35 @@ def count_pages():
     return pages_list[-1]
 
 
+def save_data(data):
+    f = open('data.txt', 'w',encoding="utf-8")
+    for el in data:
+        f.write(el['title'] + ',' + el['cost'] + '\n')
+    f.close()
+
+
 def main():
     pages = int(count_pages())
+    games = []
+    exceptions_titles = ['SILVER', 'DIAMOND - Игры от 899 рублей', 'GOLD']
     for i in range(1, pages + 1):
-        browser = webdriver.Firefox(options=options)
+        print('страница %i' %i)
+        #browser = webdriver.Firefox(options=options)
+        try
+        browser = webdriver.Firefox()
         page_url = '?page=%i' % i
         browser.get(url + page_url)
         requiredHtml = browser.page_source
         soup = BeautifulSoup(requiredHtml, 'html.parser')
         product_list = soup.select('.product-item')
-        games = []
         for product in product_list:
             title = product.select('.product-item__title > a')
             cost = product.select('.product-item__cost')
-            games.append({'title': title[0].text, 'cost': cost[0].text})
-        print(games[-1])
+            if cost[0].text != 'СКОРО' and title[0].text not in exceptions_titles:
+                games.append({'title': title[0].text, 'cost': cost[0].text})
+                print(games[-1]['title'])
         browser.quit()
+        save_data(games)
 
 
 if __name__ == "__main__":
