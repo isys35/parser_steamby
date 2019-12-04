4672dbdc633cca98b7f863dfc105ddae00c37508
isys35 <isys35@mail.com>
2019-12-04 16:24:21 +0300

    Revert "test"
    This reverts commit 1660174db593b2c748a2be09ff119d9b9d52f293.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def benchmark(func):
     return wrapper
 
 
+
 @benchmark
 def count_pages():
     print('Определение кол-ва страниц...')
@@ -56,7 +57,6 @@ def save_data_excel(data):
         ws.write(id+1, 1, data[id][keys[1]])
     wb.save('data.xls')
 
-# test git
 
 def main():
     pages = int(count_pages())
@@ -65,6 +65,7 @@ def main():
     for i in range(1, pages + 1):
         print('страница %i' %i)
         browser = webdriver.Firefox(options=options)
+        #browser = webdriver.Firefox()
         page_url = '?page=%i' % i
         browser.get(url + page_url)
         requiredHtml = browser.page_source
